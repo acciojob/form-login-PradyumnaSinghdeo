32f302d13e51286050e094f9b6427059d0b02779
Pradyumna Singh Deo <87163520+PradyumnaSinghdeo@users.noreply.github.com>
2024-07-19 01:34:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function getFormvalue() {
     //Write your code here
+	
+	const form = document.getElementById('userForm');
 
+    
+    form.addEventListener('submit', function(event) {
+        
+        event.preventDefault();
+
+       
+        const firstName = document.getElementById('fname').value;
+        const lastName = document.getElementById('lname').value;
+
+        
+        alert(`${firstName} ${lastName}`);
+    });
 }
+getFormvalue();
